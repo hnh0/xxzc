dbb589d69a713f48481c42f735b7ff5d9c70b7e8
hong.zhao <hong.zhao@autonavi.com>
2016-01-10 22:54:09 +0800

    add

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,9 +1,14 @@
 $(function(){
 	$('.index_navi a').click(function(){
-		var oThis = $(this)
+		var oThis = $(this),
+			iIdx = oThis.index();
+
 		oThis
 			.addClass('active')
 			.siblings().removeClass('active')
-		$('.J_tab').html('<div class="index_add_detail">1111111</div>')
+		// $('.J_tab').html('<div class="index_add_detail">1111111</div>')
+		$('.J_tab .tabs_item').eq(iIdx)
+			.addClass('show')
+			.siblings().removeClass('show')
 	})
 })
